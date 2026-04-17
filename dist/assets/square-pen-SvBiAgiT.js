ac459eb7e0993ed6c9bce0200a37b4a9be5889c7
root <root@srv1515643.hstgr.cloud>
2026-04-17 13:11:51 +0000

    Actualizar build dist

--- dist/assets/square-pen-SvBiAgiT.js
+++ dist/assets/square-pen-SvBiAgiT.js
@@ -1,4 +1,4 @@
-import{b as a}from"./index-BiLPtC-N.js";/**
+import{b as a}from"./index-Dmhkaijx.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
